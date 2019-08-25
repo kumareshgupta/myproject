d851b309898591402d84f72e181b489abe00a642
Kumaresh Gupta <kumaresh_gupta@yahoo.com>
2019-08-25 23:11:50 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+	public static void main(String [] args){
+			System.out.println("Welcome to Product Management System");
+			Product product = new Product(1001, "Java Primer", "Core Java Book", 599.00);
+			System.out.println(product);
+	}	
+}
